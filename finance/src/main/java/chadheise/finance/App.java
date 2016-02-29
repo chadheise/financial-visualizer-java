4f49798f0ce63c1e4d82f8ff6b138a8d08fb66fd
Chad Heise <chad@heise.org>
2016-02-28 18:31:26 -0800

    Initial checkin of project bones

---
+++ finance/src/main/java/chadheise/finance/App.java
@@ -0,0 +1,13 @@
+package chadheise.finance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
